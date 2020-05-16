c9486f89183dbf2e76db3a8964007232968460ae
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-05-15 22:11:02 -0700

    create recursion method

--- kcombination.java
+++ kcombination.java
@@ -1,3 +1,17 @@
 public class kcombination {
 
-}
+    public static int comb(int n, int k) {
+        if (k == 0) {
+            return 1;
+        } else if (k > n) {
+            return 0;
+        } else {
+            return comb(n - 1, k) + comb(n - 1, k - 1);
+        }
+        // write your code here
+    }
+
+
+/* Do not change code below */
+
+
