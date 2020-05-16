398a96d96f4f988a5dd02f365e9b191eee61bf30
yashvir <yashvir.singh_cs18@gla.ac.in>
2020-05-15 22:11:48 -0700

    method complete

--- kcombination.java
+++ kcombination.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class kcombination {
 
     public static int comb(int n, int k) {
@@ -12,6 +14,12 @@ public class kcombination {
     }
 
 
-/* Do not change code below */
-
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int k = scanner.nextInt();
+        System.out.println(comb(n, k));
+    }
+}
 
